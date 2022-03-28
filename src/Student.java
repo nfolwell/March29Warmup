fc7934e086f457b5f2e977cad3c01093ea542eb1
Nathan Folwell <nfolwell@schools.nyc.gov>
2022-03-28 15:16:50 -0400

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,16 @@
+public class Student extends Person {
+
+    private String OSIS;
+
+    public Student(String name, String OSIS) {
+// Predict which code segments will compile/run
+// then copy/paste here to try out each one
+
+
+    }
+
+    // private helper method
+    private void status() {
+        System.out.println("Setting up student...");
+    }
+}
---
+++ src/WarmupRunner.java
@@ -0,0 +1,7 @@
+public class WarmupRunner {
+        public static void main(String[] args) {
+            Student student1 = new Student("Adam", "01234567");
+        }
+    }
+
+}
